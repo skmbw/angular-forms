185b0e44fb45edfcdcf8091dc5a1634cdb47eebb
skmbw <tongku2008@126.com>
2018-04-23 11:22:38 +0800

    使用Router进行应用内路由跳转

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {User} from '../model/user';
 import {MatSnackBar} from '@angular/material';
 import {UserService} from '../service/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -12,19 +13,33 @@ export class RegisterComponent implements OnInit {
   user: User = new User();
   hide = true;
 
-  constructor(private snackBar: MatSnackBar, private userService: UserService) {
+  constructor(private snackBar: MatSnackBar, private userService: UserService, private router: Router) {
   }
 
   ngOnInit() {
   }
 
   onSubmit() {
-    if (this.user.name === undefined || this.user.name === '') {
-      this.snackBar.open('用户名不能为空！', '确定', {
+    if (this.user.account === undefined || this.user.account === '') {
+      this.snackBar.open('账户不能为空！', '确定', {
         duration: 2000,
       });
       return;
     }
+    if (this.user.password === undefined || this.user.password === '') {
+      this.snackBar.open('密码不能为空！', '确定', {
+        duration: 2000,
+      });
+      return;
+    }
+    if (this.user.password !== this.user.checkPasswd) {
+      this.snackBar.open('两次输入密码不相同！', '确定', {
+        duration: 2000,
+      });
+      return;
+    }
+    // 这里是测试，直接跳转（应用内路由跳转）
+    this.router.navigateByUrl('/login').catch();
     this.userService.register(this.user).subscribe(jsonBean => {
       if (jsonBean.code === 1) {
         console.log('register success.');
