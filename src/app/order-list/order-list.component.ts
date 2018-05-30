8919d8e50de9dc978a15895f8dbb187a7e69ba3c
skmbw <tongku2008@126.com>
2018-05-30 17:47:09 +0800

    父组件通过数据绑定，将数据传给子组件

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-order-list',
@@ -6,8 +6,11 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./order-list.component.css']
 })
 export class OrderListComponent implements OnInit {
+  @Input()
+  articleId: string = null;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
