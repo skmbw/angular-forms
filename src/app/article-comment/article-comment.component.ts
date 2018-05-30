972a839cd41dfe7344b31488b38f7d1cbc05ec6a
skmbw <tongku2008@126.com>
2018-05-30 17:46:22 +0800

    父组件通过数据绑定，将数据传给子组件

--- src/app/article-comment/article-comment.component.ts
+++ src/app/article-comment/article-comment.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-article-comment',
@@ -6,8 +6,11 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./article-comment.component.css']
 })
 export class ArticleCommentComponent implements OnInit {
+  @Input()
+  articleId: string;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
