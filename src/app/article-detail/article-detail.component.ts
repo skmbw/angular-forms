4c160db4e15e6f0adf26015376747afc970b103a
skmbw <tongku2008@126.com>
2018-05-30 17:33:59 +0800

    替换掉文章中的图片服务器前缀占位符

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -4,6 +4,7 @@ import {Article} from '../model/article';
 import {MatSnackBar} from '@angular/material';
 import {ActivatedRoute} from '@angular/router';
 import {DomSanitizer} from '@angular/platform-browser';
+import {Consts} from '../common/consts';
 
 @Component({
   selector: 'app-article-detail',
@@ -11,8 +12,9 @@ import {DomSanitizer} from '@angular/platform-browser';
   styleUrls: ['./article-detail.component.css']
 })
 export class ArticleDetailComponent implements OnInit {
-  article: Article;
+  article: Article = new Article(); // template中要显示，需要初始化，否则会找不到属性
   content: any;
+  image = {server: Consts.IMAGE_HOST};
   constructor(private articleService: ArticleService, private snackBar: MatSnackBar,
               private router: ActivatedRoute, private sanitizer: DomSanitizer) { }
 
@@ -22,7 +24,7 @@ export class ArticleDetailComponent implements OnInit {
       this.articleService.detail(id).subscribe(jsonBean => {
         if (jsonBean.code === 1) {
           this.article = jsonBean.data;
-          this.content = this.sanitizer.bypassSecurityTrustHtml(this.article.content);
+          this.content = this.sanitizer.bypassSecurityTrustHtml(this.article.content.replace('{{image.server}}', Consts.IMAGE_URL));
         } else {
           this.snackBar.open('文章不存在，亲！', '确定', {
             duration: 2000,
