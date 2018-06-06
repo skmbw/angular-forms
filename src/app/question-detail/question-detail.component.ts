c164362f1caf2d29c8d906fe53389c101693b90e
skmbw <tongku2008@126.com>
2018-06-06 17:33:41 +0800

    问题详情模组，ui调整，依赖服务注入

--- src/app/question-detail/question-detail.component.ts
+++ src/app/question-detail/question-detail.component.ts
@@ -1,15 +1,38 @@
 import {Component, OnInit} from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
+import {ActivatedRoute} from '@angular/router';
+import {QuestionService} from '../service/question.service';
+import {BaseComponent} from '../common/base.component';
+import {MatSnackBar} from '@angular/material';
+import {Consts} from '../common/consts';
+import {Question} from '../model/question';
 
 @Component({
   selector: 'app-question-detail',
   templateUrl: './question-detail.component.html',
   styleUrls: ['./question-detail.component.css']
 })
-export class QuestionDetailComponent implements OnInit {
+export class QuestionDetailComponent extends BaseComponent implements OnInit {
+  question: Question = new Question();
+  content: any = null;
 
-  constructor() { }
+  constructor(private questionService: QuestionService, private router: ActivatedRoute,
+              private sanitizer: DomSanitizer, private snackBar: MatSnackBar) {
+    super(snackBar);
+  }
 
   ngOnInit() {
+    this.router.params.subscribe(params => {
+      const id = params['id'];
+      this.questionService.detail(id).subscribe(jsonBean => {
+        if (jsonBean.code === 1) {
+          this.question = jsonBean.data;
+          this.content = this.sanitizer.bypassSecurityTrustHtml(this.question.content.replace('{{image.server}}', Consts.IMAGE_URL));
+        } else {
+          this.alert('问题不存在，亲！');
+        }
+      });
+    });
   }
 
 }
