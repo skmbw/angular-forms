98f8cd186e498ec66a165dc2c8e973e7e030b7bf
skmbw <tongku2008@126.com>
2018-06-08 09:38:48 +0800

    父子组件参数传递，什么时候能够拿到数据？？暂时先从路由中取参数吧

--- src/app/answer-list/answer-list.component.ts
+++ src/app/answer-list/answer-list.component.ts
@@ -1,33 +1,32 @@
-import {AfterContentInit, Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {AnswerService} from '../service/answer.service';
 import {Answer} from '../model/answer';
 import {BaseComponent} from '../common/base.component';
 import {MatSnackBar} from '@angular/material';
 import {Consts} from '../common/consts';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-answer-list',
   templateUrl: './answer-list.component.html',
   styleUrls: ['./answer-list.component.css']
 })
-export class AnswerListComponent extends BaseComponent implements OnInit, AfterContentInit {
+export class AnswerListComponent extends BaseComponent implements OnInit {
   @Input()
-  questionId: string;
+  questionId: string; // 这个参数在哪个生命周期方法中能够取到？页面显示是OK的
 
   answerList: Answer[] = [];
 
   imageHost = Consts.IMAGE_HOST;
 
-  constructor(private answerService: AnswerService, private snackBar: MatSnackBar) {
+  constructor(private answerService: AnswerService, private snackBar: MatSnackBar, private router: ActivatedRoute) {
     super(snackBar);
-    // alert(this.questionId);
   }
 
   ngOnInit() {
-    // 这个时候，还没有拿到@input绑定的数据
     const param: Answer = new Answer();
-    alert(this.questionId)
-    param.questionId = this.questionId;
+    // TODO 这个时候，还没有拿到@input绑定的数据，从路由里面取数据吧
+    param.questionId = this.router.snapshot.paramMap.get('id');
     this.answerService.list(param).subscribe(jsonBean => {
       if (jsonBean.code === 1) {
         this.answerList = jsonBean.data;
@@ -39,9 +38,5 @@ export class AnswerListComponent extends BaseComponent implements OnInit, AfterC
         this.alert(jsonBean.message);
       }
     });
-  }
-
-  ngAfterContentInit(): void {
-    // alert(this.questionId);
   }
 }
