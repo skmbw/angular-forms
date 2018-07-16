283a5bd6c2041ef0356ecfa6fa50cad7466febe1
skmbw <tongku2008@126.com>
2018-07-16 18:01:07 +0800

    文章评论显示

--- src/app/article-comment/article-comment.component.ts
+++ src/app/article-comment/article-comment.component.ts
@@ -1,19 +1,34 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CommentService} from '../service/comment.service';
+import {Comment} from '../model/comment';
+import {Consts} from '../common/consts';
+import {BaseComponent} from '../common/base.component';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-article-comment',
   templateUrl: './article-comment.component.html',
   styleUrls: ['./article-comment.component.css']
 })
-export class ArticleCommentComponent implements OnInit {
+export class ArticleCommentComponent extends BaseComponent implements OnInit {
 
   @Input() articleId: string;
+  commentList: Comment[] = [];
+  imageHost = Consts.IMAGE_HOST;
 
-  constructor(private commentService: CommentService) {
+  constructor(private commentService: CommentService, snackBar: MatSnackBar) {
+    super(snackBar);
   }
 
   ngOnInit() {
-    console.log('ogOnInit执行了····' + this.articleId);
+    const comment: Comment = new Comment();
+    comment.articleId = this.articleId;
+    this.commentService.list(comment).subscribe(jsonBean => {
+      if (jsonBean.code === 1) {
+        this.commentList = jsonBean.data;
+      } else {
+        this.alert(jsonBean.message);
+      }
+    });
   }
 }
