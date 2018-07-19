0aeb28d86e3f04d7c2c41022c7087a37871ebce9
skmbw <tongku2008@126.com>
2018-07-19 16:43:34 +0800

    用户中心，我的问题，我的文章列表

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,4 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ArticleService} from '../service/article.service';
+import {ToastrService} from 'ngx-toastr';
+import {Article} from '../model/article';
+import {Consts} from '../common/consts';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-article-list',
@@ -6,10 +11,23 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./article-list.component.css']
 })
 export class ArticleListComponent implements OnInit {
+  @Input()
+  userId: string;
+  articleList: Article[] = [];
+  imageHost = Consts.IMAGE_HOST;
+  faBars = faBars;
 
-  constructor() { }
+  constructor(private articleService: ArticleService, private toastr: ToastrService) {
+  }
 
   ngOnInit() {
+    this.articleService.list(1).subscribe(jsonBean => {
+      if (jsonBean.code === 1) {
+        this.articleList = jsonBean.data;
+      } else {
+        this.toastr.info(jsonBean.message);
+      }
+    });
   }
 
 }
--- src/app/question-list/question-list.component.ts
+++ src/app/question-list/question-list.component.ts
@@ -1,4 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {QuestionService} from '../service/question.service';
+import {ToastrService} from 'ngx-toastr';
+import {Consts} from '../common/consts';
+import {Question} from '../model/question';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-question-list',
@@ -6,10 +11,23 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./question-list.component.css']
 })
 export class QuestionListComponent implements OnInit {
+  @Input()
+  userId: string;
+  imageHost = Consts.IMAGE_HOST;
+  questionList: Question[] = [];
+  faBars = faBars;
 
-  constructor() { }
+  constructor(private questionService: QuestionService, private toastr: ToastrService) {
+  }
 
   ngOnInit() {
+    this.questionService.list({}).subscribe(jsonBean => {
+      if (jsonBean.code === 1) {
+        this.questionList = jsonBean.data;
+      } else {
+        this.toastr.info(jsonBean.message);
+      }
+    });
   }
 
 }
