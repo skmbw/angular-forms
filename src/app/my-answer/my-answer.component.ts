5f76e1270dfa6abba6183ce3a6bf595183a69e93
skmbw <tongku2008@126.com>
2018-07-22 15:48:01 +0800

    我的回答列表

--- src/app/my-answer/my-answer.component.ts
+++ src/app/my-answer/my-answer.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {AnswerService} from '../service/answer.service';
+import {Answer} from '../model/answer';
+import {Consts} from '../common/consts';
+import {TokenStorage} from '../token/token.storage';
 
 @Component({
   selector: 'app-my-answer',
@@ -6,10 +10,26 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./my-answer.component.css']
 })
 export class MyAnswerComponent implements OnInit {
+  answerList: Answer[] = [];
+  imageHost = Consts.IMAGE_HOST;
 
-  constructor() { }
+  constructor(private answerService: AnswerService, private tokenStorage: TokenStorage) {
+  }
 
   ngOnInit() {
+    const param: Answer = new Answer();
+    param.answerUserId = this.tokenStorage.getUserId();
+    this.answerService.list(param).subscribe(jsonBean => {
+      if (jsonBean.code === 1) {
+        this.answerList = jsonBean.data;
+        for (const answer of this.answerList) {
+          answer.content = answer.content.replace('localhost:8300/', Consts.IMAGE_URL)
+            .replace('{{image.server}}', Consts.IMAGE_URL);
+        }
+      } else {
+        // this.alert('还没有人回答，赶紧去抢答！');
+      }
+    });
   }
 
 }
