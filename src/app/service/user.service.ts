78ba64ecec8bda0362af373d9068a07836be451c
skmbw <tongku2008@126.com>
2018-07-22 21:37:49 +0800

    上传头像组件，如何正确的设置content-type

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -42,8 +42,12 @@ export class UserService extends CommonService {
     const data = new FormData();
     data.append('userId', userId);
     data.append('avatarFile', image);
-    // 上传文件不要自作多情去添加头部 no multipart boundary was found
+    // 上传文件不要去添加头部 no multipart boundary was found
     // https://blog.csdn.net/sanjay_f/article/details/47407063
+    // 那些觉得错误是自作多情造成的同学一看就是没有理解文件上传原理的，错误明明告诉你了没有设置boundary边界，
+    // 正确的消息头应该是'Content-Type':'multipart/form-data;boundary='+随机数,
+    // 而误打误撞删了Content-Type就成功的同学只能说浏览器默认帮你加上了正确的消息头运气好而已，
+    // 如果Content-Typ默认不是multipart/form-data类型的又会报Current request is not a multipart request的错误
     return this.httpClient.post<JsonBean>(Consts.URL + 'user/avatar', data);
   }
 }
