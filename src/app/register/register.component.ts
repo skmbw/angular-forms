cb90a6392748809ec675dae1bd4ed4670ce614b9
skmbw <tongku2008@126.com>
2018-08-14 11:02:18 +0800

    限制用户注册的密码长度最少为8位

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -32,6 +32,12 @@ export class RegisterComponent implements OnInit {
       });
       return;
     }
+    if (this.user.password.length < 8) {
+      this.snackBar.open('密码长度最少为8位！', '确定', {
+        duration: 2000,
+      });
+      return;
+    }
     if (this.user.password !== this.user.checkPasswd) {
       this.snackBar.open('两次输入密码不相同！', '确定', {
         duration: 2000,
@@ -44,18 +50,17 @@ export class RegisterComponent implements OnInit {
       });
       return;
     }
-    // 这里是测试，直接跳转（应用内路由跳转）
-    // this.router.navigateByUrl('/login').catch();
-    // this.userService.register(this.user).subscribe(jsonBean => {
-    //   if (jsonBean.code === 1) {
-    //     this.snackBar.open('注册成功，请登录，2秒后跳转。', '确定', {duration: 2000});
-    //     // 两秒后跳转
-    //     setTimeout(() => {
-    //       this.router.navigateByUrl('/login').catch();
-    //     }, 2000);
-    //   } else {
-    //     this.snackBar.open(jsonBean.message, '确定', {duration: 2000});
-    //   }
-    // });
+    // 应用内路由跳转
+    this.userService.register(this.user).subscribe(jsonBean => {
+      if (jsonBean.code === 1) {
+        this.snackBar.open('注册成功，请登录，2秒后跳转。', '确定', {duration: 2000});
+        // 两秒后跳转
+        setTimeout(() => {
+          this.router.navigateByUrl('/login').catch();
+        }, 2000);
+      } else {
+        this.snackBar.open(jsonBean.message, '确定', {duration: 2000});
+      }
+    });
   }
 }
